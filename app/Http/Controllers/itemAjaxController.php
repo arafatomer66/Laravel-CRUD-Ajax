b0a4345cc71ad44a8ac45d461242fe96d624f8af
Md Omer Arafat <arafatomer66@gmail.com>
2018-08-30 15:34:04 +0600

    Initial commit

---
+++ app/Http/Controllers/itemAjaxController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Item;
+
+use Illuminate\Http\Request;
+
+class itemAjaxController extends Controller
+{
+    public function manageItemAjax()
+    {
+        return view('manage-item-ajax');
+    }
+
+
+  
+    public function index(Request $request)
+    {
+        $items = Item::latest()->paginate(5);
+        return response()->json($items);
+    }
+
+
+  
+    public function store(Request $request)
+    {
+        $create = Item::create($request->all());
+        return response()->json($create);
+    }
+
+
+ 
+    public function update(Request $request, $id)
+    {
+        $edit = Item::find($id)->update($request->all());
+        return response()->json($edit);
+    }
+
+
+    public function destroy($id)
+    {
+        Item::find($id)->delete();
+        return response()->json(['done']);
+    }
+}
